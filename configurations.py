bcee8b211cfb37d6b8bd91b8f5d0426865c461e3
yhmtxt <yhmtxt@163.com>
2025-03-15 17:15:25 +0800

    20250315-1

--- configurations.py
+++ configurations.py
@@ -4,8 +4,16 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL")
 
-JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
+def get_env_var(key: str) -> str:
+    value = os.getenv(key)
+    if value is None:
+        raise RuntimeError(f"Missing required environment variable: {key}")
+    return value
+
+
+DATABASE_URL = get_env_var("DATABASE_URL")
+
+JWT_SECRET_KEY = get_env_var("JWT_SECRET_KEY")
 JWT_ALGORITHM = "HS256"
 DEFAULT_ACCESS_TOKEN_EXPIRE_DAYS = 7
